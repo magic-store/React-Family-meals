bfe785f075bbad91d42090aa4a153eb6bf62f3ac
weiya <jiaweiya@didichuxing.com>
2018-07-05 21:08:24 +0800

    wip

--- src/app/one/one.component.tsx
+++ src/app/one/one.component.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { Two } from '../two'
 import * as Rx from 'rxjs'
-
-// import { take, map, combineAll } from 'rxjs/operators'
-
-// import * as interval from 'rxjs/add/observable/interval'
-
+import './one.component.styl'
 export class One extends React.PureComponent<any, any> {
   constructor(props) {
     super(props)
@@ -14,21 +10,21 @@ export class One extends React.PureComponent<any, any> {
     }
   }
 
+  timer: any
+
   componentDidMount() {
-    // console.dir(this)
-    setTimeout(() => {
-      this.setState({
-        arr: [0, 2, 3, 4]
-      })
-    }, 3000)
     let counter = 0
-    setInterval(() => {
+    this.timer = setInterval(() => {
       this.setState({
         arr: [0, 2, 3, counter++]
       })
     }, 1000)
 
     this.setObservable()
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
   }
 
   setObservable = () => {
@@ -42,34 +38,6 @@ export class One extends React.PureComponent<any, any> {
     })
     demo.take(1).subscribe(r => console.info(r, demo))
 
-    // 每秒发出值，并只取前2个
-    // const source = interval(1000).pipe(take(2))
-    // // 将 source 发出的每个值映射成取前5个值的 interval observable
-    // const example = source.pipe(
-    //   map(val =>
-    //     interval(1000).pipe(map(i => `Result (${val}): ${i}`), take(5))
-    //   )
-    // )
-    /*
-  soure 中的2个值会被映射成2个(内部的) interval observables，
-  这2个内部 observables 每秒使用 combineLatest 策略来 combineAll，
-  每当任意一个内部 observable 发出值，就会发出每个内部 observable 的最新值。
-*/
-    //const combined = example.pipe(combineAll())
-    /*
-  输出:
-  ["Result (0): 0", "Result (1): 0"]
-  ["Result (0): 1", "Result (1): 0"]
-  ["Result (0): 1", "Result (1): 1"]
-  ["Result (0): 2", "Result (1): 1"]
-  ["Result (0): 2", "Result (1): 2"]
-  ["Result (0): 3", "Result (1): 2"]
-  ["Result (0): 3", "Result (1): 3"]
-  ["Result (0): 4", "Result (1): 3"]
-  ["Result (0): 4", "Result (1): 4"]
-*/
-    // const subscribe = combined.subscribe(val => console.log(val))
-    // console.info(subscribe)
     const timeOut = ms => {
       return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -84,19 +52,9 @@ export class One extends React.PureComponent<any, any> {
         return r
       })
       console.info(222, zhiHu)
-      this.updateState('222')
     }
 
     print(3000)
-  }
-
-  updateState = (newOne: string) => {
-    console.dir(this)
-    const newArr = [...this.state.arr, newOne]
-
-    this.setState({
-      arr: newArr
-    })
   }
 
   handleClick = () => {
--- src/root/root.component.tsx
+++ src/root/root.component.tsx
@@ -3,8 +3,6 @@ import * as React from 'react'
 import * as Styles from './root.component.styl'
 import { Link } from 'react-router-dom'
 
-// import { One, Two, Three } from '../app/index'
-
 import { MapDispatchProps, MapOwnProps, MapStateProps } from './root.container'
 import { action } from './root.module'
 
@@ -30,31 +28,6 @@ export class RootComponent extends React.PureComponent<Readonly<props>> {
             </div>
           )
         })}
-        {/* <Route exact={true} path="/two" component={Two} />
-        <Route exact={true} path="/three" component={Three} />
-        <Route exact={true} path="/one" component={One} /> */}
-        <a href="https://www.baidu.com">222</a>
-
-        <div className={Styles.wrap}>
-          <div>
-            <span>1</span>
-          </div>
-          <div>
-            <span>2</span>
-          </div>
-          <div>
-            <span>3</span>
-          </div>
-          <div>
-            <span>4</span>
-          </div>
-          <div>
-            <span>5</span>
-          </div>
-          <div>
-            <span>6</span>
-          </div>
-        </div>
       </React.Fragment>
     )
   }
